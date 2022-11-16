1c224ffaa72a2de84f6385cd67a5f2b5ac57a67d
msxie92 <34064199+msxie92@users.noreply.github.com>
2022-11-16 17:30:30 +0800

    Update screenvae.py

--- ScreenVAE/models/screenvae.py
+++ ScreenVAE/models/screenvae.py
@@ -53,31 +53,31 @@ class ScreenVAE(nn.Module):
             assert(torch.cuda.is_available())
             self.gaborext.to(gpu_ids[0])
 
-    def npad(self, im, pad=128, value=0):
+    def npad(self, im, pad=128):
         h,w = im.shape[-2:]
         hp = h //pad*pad+pad
         wp = w //pad*pad+pad
-        return F.pad(im, (0, wp-w, 0, hp-h), mode='constant',value=value)
+        return F.pad(im, (0, wp-w, 0, hp-h), mode='replicate')
 
-    def forward(self, x, line=None, screen=False, rep=False):
+    def forward(self, x, line=None, img_input=True, output_screen_only=True):
         if line is None:
             line = torch.ones_like(x)
         else:
             line = torch.sign(line)
             x = torch.clamp(x + (1-line),-1,1)
-        if not screen:
+        if img_input:
             h,w = x.shape[-2:]
             input = torch.cat([x, line], 1)
-            input = self.npad(input,value=1)
+            input = self.npad(input)
             inter = self.enc(input)[:,:,:h,:w]
             scr, logvar = torch.split(inter, (self.outc, self.outc), dim=1)
-            if rep:
+            if output_screen_only:
                 return scr
             recons = self.dec(scr)
             return recons, scr
         else:
             h,w = x.shape[-2:]
-            x = self.npad(x,value=0)
+            x = self.npad(x)
             recons = self.dec(x)[:,:,:h,:w]
             recons = (recons+1)*(line+1)/2-1
             return torch.clamp(recons,-1,1)
